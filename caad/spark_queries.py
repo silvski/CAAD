72be7f731341eff27dfd8c8cf2b632939d523c89
Nigel Silva <silva.n.ct.ds@gmail.com>
2024-07-21 15:05:07 +0800

    added spark queries module

--- caad/spark_queries.py
+++ caad/spark_queries.py
@@ -1,3 +1,93 @@
+from typing import List
+
+from pyspark.sql import Column
+from pyspark.sql import functions as f
+from pyspark.sql.types import LongType
+
 from caad.spark_utils import TemporalWindow as tw
 
-# tw.create_lookahead_window()
+
+def lagged_delta(
+    minuend_col: str | Column,
+    subtrahend_col: str | Column,
+    timestamp_col: str | Column,
+    partition_col: str | List[str] | Column,
+    window_length: int,
+    offset: int,
+    unit_of_time: str,
+    subtrahend_aggregation: str,
+) -> Column:
+
+    if isinstance(minuend_col, str):
+        minuend_col = f.col(minuend_col)
+    if isinstance(subtrahend_col, str):
+        subtrahend_col = f.col(subtrahend_col)
+    if isinstance(timestamp_col, str):
+        timestamp_col = f.col(timestamp_col)
+
+    def aggregation_strategies(col):
+        return {
+            "mean": f.mean(col),
+            "last": f.last(col, ignorenulls=True),
+            "first": f.first(col, ignorenulls=True),
+            "median": f.percentile_approx(col, 0.5),
+        }
+
+    valid_agg_strategies = aggregation_strategies(subtrahend_col).keys()
+
+    subtrahend_col = aggregation_strategies(subtrahend_col).get(
+        subtrahend_aggregation, None
+    )
+
+    if subtrahend_col is None:
+        raise ValueError(
+            f"Invalid aggregation strategy; may only be one of: {valid_agg_strategies}."
+        )
+
+    w = tw.create_lookback_window(
+        window_length=window_length,
+        offset=offset,
+        unit_of_time=unit_of_time,
+        timestamp_col=timestamp_col,
+        partition_col=partition_col,
+    )
+
+    return minuend_col - subtrahend_col.over(w)
+
+
+def rate_of_change(
+    col_name: str | Column,
+    timestamp_col: str | Column,
+    partition_col: str | List[str] | Column,
+    window_length: int,
+    offset: int,
+    unit_of_time: str,
+    subtrahend_aggregation: str,
+) -> Column:
+
+    if isinstance(timestamp_col, str):
+        timestamp_col = f.col(timestamp_col)
+
+    numerator = lagged_delta(
+        minuend_col=col_name,
+        subtrahend_col=col_name,
+        timestamp_col=timestamp_col,
+        partition_col=partition_col,
+        window_length=window_length,
+        offset=offset,
+        unit_of_time=unit_of_time,
+        subtrahend_aggregation=subtrahend_aggregation,
+    )
+
+    denominator = lagged_delta(
+        minuend_col=timestamp_col.cast(LongType),
+        subtrahend_col=timestamp_col.cast(LongType),
+        timestamp_col=timestamp_col,
+        partition_col=partition_col,
+        window_length=window_length,
+        offset=offset,
+        unit_of_time=unit_of_time,
+        subtrahend_aggregation="latest",
+    )
+
+    return numerator / denominator
--- caad/spark_utils.py
+++ caad/spark_utils.py
@@ -1,8 +1,12 @@
+from typing import List
+
 from pyspark.sql import Column, Window
+from pyspark.sql import functions as f
+from pyspark.sql.types import LongType
 
 
 class TemporalWindow:
-    resolution = {
+    mapping = {
         "days": 86400,
         "hours": 3600,
         "weeks": 604800,
@@ -15,100 +19,104 @@ class TemporalWindow:
         if window_length < 0:
             raise ValueError("Window size must not be negative.")
 
-        multiplier = TemporalWindow.resolution.get(unit_of_time, None)
+        multiplier = TemporalWindow.mapping.get(unit_of_time, None)
 
         if multiplier is None:
             raise ValueError(
-                f"Unit of time must be one of: {TemporalWindow.resolution.keys()}"
+                f"Unit of time must be one of: {TemporalWindow.mapping.keys()}"
             )
 
         return multiplier * window_length
 
     @staticmethod
-    def create_lookback_window(
-        window_length: int,
+    def create_window(
+        lookback_length: int,
+        lookahead_length: int,
+        lookback_offset: int,
+        lookahead_offset: int,
         unit_of_time: str,
-        order_col: list[str] | str | Column,
-        partition_col: list[str] | str | Column,
-        include_current: bool = True,
-        offset: int = 0
+        timestamp_col: Column,
+        partition_col: str | List[str] | Column,
+        include_start_bound: bool = True,
+        include_end_bound: bool = True,
     ):
-        """Return a pyspark `WindowSpec` object that's configured to perform a lookback operation across time,
-        inclusive of the current time.
 
-        Args:
-            window_length (int): The length of the window.
-            unit_of_time (str): The unit of time for the window length.
-            order_col (list[str] | str | Column): The column to order the window by.
-            partition_col (list[str] | str | Column): The column to partition the window by.
-            include_current (bool, optional): Boolean flag that indicates if current row is included. Defaults to True.
-            offset (int, optional): The offset to apply to the bounds of the window.
+        int_params = [
+            lookback_length,
+            lookahead_length,
+            lookback_offset,
+            lookahead_offset,
+        ]
+        lookback_length, lookahead_length, lookback_offset, lookahead_offset = [
+            TemporalWindow.convert_window_length_to_seconds(
+                window_length=param, unit_of_time=unit_of_time
+            )
+            for param in int_params
+        ]
 
-        Returns:
-            pyspark.sql.Window: A window to be used by pyspark functions.
-        """
-
-        lookback_window_length = TemporalWindow.convert_window_length_to_seconds(
-            window_length=window_length, unit_of_time=unit_of_time
-        )
-        rescaled_offset = TemporalWindow.convert_window_length_to_seconds(
-            window_length=offset, unit_of_time=unit_of_time
-        )
-
-        start_bound = -(lookback_window_length + rescaled_offset)
-
-        if include_current:
-            end_bound = Window.currentRow - rescaled_offset
+        if include_start_bound:
+            start_bound = -(lookback_length + lookback_offset)
         else:
-            end_bound = Window.currentRow - 1 - rescaled_offset
+            start_bound = -(lookback_length + lookback_offset) + 1
+
+        if include_end_bound:
+            end_bound = lookahead_length + lookahead_offset
+        else:
+            end_bound = lookahead_length + lookahead_offset - 1
 
         return (
             Window.partitionBy(partition_col)
-            .orderBy(order_col)
+            .orderBy(timestamp_col.cast(LongType))
             .rangeBetween(start=start_bound, end=end_bound)
+        )
+
+    @staticmethod
+    def create_lookback_window(
+        window_length: int,
+        offset,
+        unit_of_time: str,
+        timestamp_col: str | Column,
+        partition_col: str | List[str] | Column,
+        include_start_bound: bool = True,
+        include_end_bound: bool = True,
+    ):
+        """Return a pyspark `WindowSpec` object that's configured to perform a lookback operation across time,
+        inclusive of the current time.
+        """
+
+        return TemporalWindow.create_window(
+            lookback_length=window_length,
+            lookahead_length=Window.currentRow,
+            lookback_offset=offset,
+            lookahead_offset=0,
+            unit_of_time=unit_of_time,
+            timestamp_col=timestamp_col,
+            partition_col=partition_col,
+            include_start_bound=include_start_bound,
+            include_end_bound=include_end_bound,
         )
 
     @staticmethod
     def create_lookahead_window(
         window_length: int,
+        offset: int,
         unit_of_time: str,
-        order_col: list[str] | str | Column,
-        partition_col: list[str] | str | Column,
-        include_current: bool = False,
-        offset: int = 0
+        timestamp_col: str | Column,
+        partition_col: str | List[str] | Column,
+        include_start_bound: bool = False,
+        include_end_bound: bool = True,
     ):
         """Return a pyspark `WindowSpec` object that's configured to perform a lookahead operation across time,
         exclusive of the current time.
-
-        Args:
-            window_length (int): The length of the window.
-            unit_of_time (str): The unit of time for the window length.
-            order_col (list[str] | str | Column): The column to order the window by.
-            partition_col (list[str] | str | Column): The column to partition the window by.
-            include_current (bool, optional): Boolean flag that indicates if current row is included. Defaults to True.
-            offset (int, optional): The offset to apply to the bounds of the window.
-
-        Returns:
-            pyspark.sql.Window: A window to be used by pyspark functions.
         """
-
-        lookahead_window_length = TemporalWindow.convert_window_length_to_seconds(
-            window_length=window_length, unit_of_time=unit_of_time
-        )
-
-        rescaled_offset = TemporalWindow.convert_window_length_to_seconds(
-            window_length=offset, unit_of_time=unit_of_time
-        )
-
-        end_bound = lookahead_window_length + rescaled_offset
-
-        if include_current:
-            start_bound = Window.currentRow + rescaled_offset
-        else:
-            start_bound = Window.currentRow + 1 + rescaled_offset
-
-        return (
-            Window.partitionBy(partition_col)
-            .orderBy(order_col)
-            .rangeBetween(start=start_bound, end=end_bound)
+        return TemporalWindow.create_window(
+            lookback_length=Window.currentRow,
+            lookahead_length=window_length,
+            lookback_offset=0,
+            lookahead_offset=offset,
+            unit_of_time=unit_of_time,
+            timestamp_col=timestamp_col,
+            partition_col=partition_col,
+            include_start_bound=include_start_bound,
+            include_end_bound=include_end_bound,
         )
